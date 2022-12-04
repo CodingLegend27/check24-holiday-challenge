f48ca2c87bd51140e8f8278d0ce03120e482b120
Christoph Waffler <waffler.christoph@gmail.com>
2022-12-04 21:32:37 +0100

    add new date formattings

--- website/lib/util/date_format.dart
+++ website/lib/util/date_format.dart
@@ -3,4 +3,9 @@ DateTime now = DateTime.now();
 extension DateFormatter on DateTime {
   get yearMonthDay =>
       "${year.toString()}-${month.toString().padLeft(2, '0')}-${day.toString().padLeft(2, '0')}";
+
+  get dayMonthYear =>
+      "${day.toString().padLeft(2, '0')}.${month.toString().padLeft(2, '0')}.${year.toString()}";
+
+  get time => "$hour:$minute";
 }
