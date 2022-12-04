52290f8aa114f474a682d2b5638ba10735aa716c
Christoph Waffler <waffler.christoph@gmail.com>
2022-12-04 20:32:45 +0100

    add IATA converting

--- server/lib/app.py
+++ server/lib/app.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+from iata_converter import IATAConverter
 
 from utils import data_to_hotel, data_to_offer
 
@@ -9,6 +10,8 @@ app = Flask(__name__)
 CORS(app)
 
 RESULTS_SIZE = 10
+
+iata_converter = IATAConverter()
 
 
 def get_db_connection():
@@ -26,8 +29,8 @@ def query():
     data = request.get_json()
     earliest_start = data['earliest_start']
     latest_end = data['latest_end']
-    starting_airport = data['starting_airport']
-    destination_airport = data['destination_airport']
+    starting_airport = iata_converter.get_airport_by_IATA(data['starting_airport'])
+    destination_airport = iata_converter.get_airport_by_IATA(data['destination_airport'])
     count_adults = data['count_adults']
     count_children = data['count_children']
     duration = data['duration']
@@ -50,7 +53,7 @@ def query():
                   'destination_airport': destination_airport, 'count_adults': count_adults, 'count_children':
                   count_children, 'duration': duration})
     data = cursor.fetchmany(RESULTS_SIZE)
-    offers = [data_to_offer(d) for d in data]
+    offers = [data_to_offer(data=d, converter=iata_converter) for d in data]
     print(offers)
     return jsonify(offers)
 
@@ -60,8 +63,18 @@ def departure_airports():
     connection = get_db_connection()
     connection.row_factory = lambda cursor, row: row[0]
     cursor = connection.cursor()
+
     cursor.execute("SELECT DISTINCT outbounddepartureairport FROM offers")
-    return jsonify(cursor.fetchall())
+    iata_codes = cursor.fetchall()
+    
+    print(iata_codes)
+
+    # convert IATA codes to airport names
+    destinations = [
+        iata_converter.get_airport_by_IATA(iata_code) for iata_code in iata_codes
+    ]
+
+    return jsonify(destinations)
 
 
 @app.get('/destinations')
@@ -69,8 +82,16 @@ def destinations():
     connection = get_db_connection()
     connection.row_factory = lambda cursor, row: row[0]
     cursor = connection.cursor()
+
     cursor.execute("SELECT DISTINCT outboundarrivalairport FROM offers")
-    return jsonify(cursor.fetchall())
+    iata_codes = cursor.fetchall()
+
+    # convert IATA codes to airport names
+    destinations = [
+        iata_converter.get_airport_by_IATA(iata_code) for iata_code in iata_codes
+    ]
+
+    return jsonify(destinations)
 
 
 @app.get('/hotel/<id>')
@@ -89,6 +110,6 @@ def hotel_offers(id):
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM offers WHERE hotelid = :id", {'id': id})
     data = cursor.fetchmany(RESULTS_SIZE)
-    offers = [data_to_offer(d) for d in data]
+    offers = [data_to_offer(data=d, converter=iata_converter) for d in data]
     print(offers)
     return jsonify(offers)
--- server/lib/utils.py
+++ server/lib/utils.py
@@ -1,5 +1,33 @@
-def data_to_offer(data):
-    return {'hotelid': (int) (data[0]), 'departuredate': data[1], 'returndate': data[2], 'countadults': (int) (data[3]), 'countchildren': (int) (data[4]), 'price': (int) (data[5]), 'inbounddepartureairport': data[6], 'inboundarrivalairport': data[7], 'inboundairline': data[8], 'inboundarrivaldatetime': data[9], 'outbounddepartureairport': data[10], 'outboundarrivalairport': data[11], 'outboundairline': data[12], 'outboundarrivaldatetime': data[13], 'mealtype': data[14], 'oceanview': (bool) (data[15]), 'roomtype': data[16]}
+from iata_converter import IATAConverter
+
+
+def data_to_offer(data, converter: IATAConverter):
+    return {
+        'hotelid': (int)(data[0]),
+        'departuredate': data[1],
+        'returndate': data[2],
+        'countadults': (int)(data[3]),
+        'countchildren': (int)(data[4]),
+        'price': (int)(data[5]),
+        'inbounddepartureairport': converter.get_airport_by_IATA(data[6]),
+        'inboundarrivalairport': converter.get_airport_by_IATA(data[7]),
+        'inboundairline': converter.get_airline_by_IATA(data[8]),
+        'inboundarrivaldatetime': data[9],
+        'outbounddepartureairport': converter.get_airport_by_IATA(data[10]),
+        'outboundarrivalairport': converter.get_airport_by_IATA(data[11]),
+        'outboundairline': converter.get_airline_by_IATA(data[12]),
+        'outboundarrivaldatetime': data[13],
+        'mealtype': data[14],
+        'oceanview': (bool)(data[15]),
+        'roomtype': data[16]
+    }
+
 
 def data_to_hotel(data):
-    return {'hotelid': (int) (data[0]), 'hotelname': data[1], 'latitude': (float) (data[2]), 'longitude': (float) (data[3]), 'hotelstars': (float) (data[4])}
+    return {
+        'hotelid': (int)(data[0]),
+        'hotelname': data[1],
+        'latitude': (float)(data[2]),
+        'longitude': (float)(data[3]),
+        'hotelstars': (float)(data[4])
+    }
